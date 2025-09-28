159aec8e463beb9cc6cfc4da76ad8e8348972054
fátima <fatimacatacata51@gmail.com>
2025-09-28 20:38:06 -0300

    feat: Ya resolvimos el problema del movimiento desorbitado. Y funciona todo correctamente

--- Assets/Prefabs/Player/Model/MovimientoLateral.cs
+++ Assets/Prefabs/Player/Model/MovimientoLateral.cs
@@ -5,6 +5,6 @@ public class MovimientoLateral : IMovementStrategy
     public void Move(Transform transform, float velocidad)
     {
         float direccion = Input.GetAxis("Horizontal");
-        transform.Translate(direccion + velocidad + Time.deltaTime, 0,0);
+        transform.Translate(direccion * velocidad * Time.deltaTime, 0,0);
     }
 }
